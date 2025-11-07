bac434762db92ac8c699f747ff959cd3d077a767
Bohdan <bohdan.voiyov@gmail.com>
2025-11-07 19:01:14 +0200

    fix_views

--- HomeWork/hw1/lesson_11/views.py
+++ HomeWork/hw1/lesson_11/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def lesson1_view(request):
+    """Повертає сторінку lesson_1"""
     # simple Hello World for /lesson_1/
     return render(request, 'lesson_1.html')
 
 def hello_page(request):
+    """Повертає сторінку hello"""
     # for lesson_1_1/ handled via include
     return render(request, 'lesson_1_1.html')
